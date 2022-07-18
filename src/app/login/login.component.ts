83737773455e6e163f4cf413546e1e4a92c98468
mohammedShafaz <shafazfaizal@gmail.com>
2022-07-18 20:59:56 +0530

    event binding using

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   vision="The Future Is Here...."
-  acnum="Account number please..!"
+  acnum="Account number please...!"
   acno=""
   pswd=""
   //database
-  database={
+  database:any={
     1000:{acno:1000,uname:"shafaz",password:1000,balance:5000},
     1001:{acno:1001,uname:"rachel",password:1001,balance:4000},
     1002:{acno:1002,uname:"milan",password:1002,balance:7000},
@@ -24,15 +24,43 @@ export class LoginComponent implements OnInit {
   }
 
   acnoChange(event:any){
+  
+    
+    
     this.acno=event.target.value
   }
+
   pswdChange(event: any){
     this.pswd=event.target.value
   }
+
+  // login-using event binding
+
   login(){
-    alert("Login clicked..!!")
+    // alert("Login clicked..!!")
+  var acno= this.acno
+  var pswd=this.pswd
+  var database=this.database
+  if(acno in database){
+    if(pswd == database[acno]["password"])
+    {
+      alert("Login Succesfull..!")
+
+
+    }
+    else{
+      alert("Incorrect password...!")
+    }
+  
+
+  }
+  else{
+    alert("User does not exist....!")
+  }
   
   }
+  
+
 
 
 
